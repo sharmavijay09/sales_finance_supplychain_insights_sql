5d8d088e88693aaaa1409ba2d9b2507f4973cf71
Vijay Sharma <169172924+sharmavijay09@users.noreply.github.com>
2024-08-28 16:26:32 +0530

    Add files via upload

---
+++ Sql_Chapter7_queries_monthly_product_transactions.sql
@@ -0,0 +1,16 @@
+select
+	s.date, s.product_code,
+    p.product, p.variant, s.sold_quantity,
+    g.gross_price as gross_price_per_item,
+    round(g.gross_price * s.sold_quantity, 2) as gross_price_total
+from fact_sales_monthly s
+join dim_product p
+on p.product_code = s.product_code
+join fact_gross_price g
+on 
+	g.product_code = s.product_code and 
+	g.fiscal_year = get_fiscal_year(s.date)
+where
+	customer_code = 90002002 and get_fiscal_year(date) = 2021
+    and get_fiscal_quarter(date) = "q4"
+    order by date asc;
